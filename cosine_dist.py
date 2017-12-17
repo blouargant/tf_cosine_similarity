7b91ccb31021d4b7b684c30473d37be881a2ef3b
Bertrand Louargant <bertrand.louargant@atoutlinux.net>
2017-12-17 20:12:00 +0100

    better solution

--- cosine_dist.py
+++ cosine_dist.py
@@ -24,10 +24,24 @@ input4 = doc3.vector
 #z = tf.constant(input3)
 #with tf.device('/device:CPU:0'):
 sess = tf.Session()
-
+"""
 x = tf.placeholder(tf.float32)
 y = tf.placeholder(tf.float32)
 dist = (1 - tf.losses.cosine_distance(tf.nn.l2_normalize(x, 0), tf.nn.l2_normalize(y, 0), dim=0))
 
 res = [sess.run(dist, feed_dict={x:ref, y:input_val}) for input_val in [input1, input2, input3, input4]]
 print(res)
+"""
+#b = tf.placeholder(tf.float32, shape=[None, 384], name="input_placeholder_b")
+#with tf.device('/device:GPU:0'):
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+normalize_a = tf.nn.l2_normalize(a, dim=1)
+normalize_b = tf.nn.l2_normalize(b, dim=1)
+#cos_similarity=tf.reduce_sum(tf.multiply(normalize_a,normalize_b))
+cos_similarity = tf.matmul(normalize_a, tf.transpose(normalize_b, [1, 0]))
+#cos_similarity = tf.argmax(cos_similarity, 1)
+cos_sim=sess.run(cos_similarity,feed_dict={a:[ref],b:[input1,input2,input3,input4]})
+print("result: %s" % cos_sim)
+
+print(np.argmax(cos_sim))
